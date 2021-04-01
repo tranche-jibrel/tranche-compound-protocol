48652d094a3344115d741bc6fd77c0448e426668
Saurabh <sw@tranche.finance>
2021-04-01 13:22:11 +0400

    update flatten script

--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -8,22 +8,6 @@ fi
 
 mkdir dist
 # TO-DO: Comments (author, summary, Created On) should be handled better.
-# JFactory
-echo "Flattening # JLoanHelper contract"
-npx truffle-flattener ./contracts/JLoanHelper.sol | awk '/SPDX-License-Identifier/&&c++>0 {next} 1' | awk '/pragma experimental ABIEncoderV2;/&&c++>0 {next} 1' | awk '/pragma solidity/&&c++>0 {next} 1' | awk '/author/&&c++>0 {next} 1' | awk '/summary/&&c++>0 {next} 1' | awk '/Created on/&&c++>0 {next} 1' | sed '/^[[:blank:]]*\/\/ File/d;s/#.*//' >./dist/JLoanHelper.sol
-
-# JFeesCollector
-echo "Flattening # JFeesCollector contract"
-npx truffle-flattener ./contracts/JFeesCollector.sol | awk '/SPDX-License-Identifier/&&c++>0 {next} 1' | awk '/pragma experimental ABIEncoderV2;/&&c++>0 {next} 1' | awk '/pragma solidity/&&c++>0 {next} 1' | awk '/author/&&c++>0 {next} 1' | awk '/summary/&&c++>0 {next} 1' | awk '/Created on/&&c++>0 {next}  1' | sed '/^[[:blank:]]*\/\/ File/d;s/#.*//' >./dist/JFeesCollector.sol
-
-# JLoanDeployer
-echo "Flattening # JLoan contract"
-npx truffle-flattener ./contracts/JLoan.sol | awk '/SPDX-License-Identifier/&&c++>0 {next} 1' | awk '/pragma experimental ABIEncoderV2;/&&c++>0 {next} 1' | awk '/pragma solidity/&&c++>0 {next} 1' | awk '/author/&&c++>0 {next} 1' | awk '/summary/&&c++>0 {next} 1' | awk '/Created on/&&c++>0 {next} 1' | sed '/^[[:blank:]]*\/\/ File/d;s/#.*//' >./dist/JLoan.sol
-
-# JPriceOracle
-echo "Flattening # JPriceOracle contract"
-npx truffle-flattener ./contracts/JPriceOracle.sol | awk '/SPDX-License-Identifier/&&c++>0 {next} 1' | awk '/pragma experimental ABIEncoderV2;/&&c++>0 {next} 1' | awk '/pragma solidity/&&c++>0 {next} 1' | awk '/author/&&c++>0 {next} 1' | awk '/summary/&&c++>0 {next} 1' | awk '/Created on/&&c++>0 {next} 1' | sed '/^[[:blank:]]*\/\/ File/d;s/#.*//' >./dist/JPriceOracle.sol
-
-# Jtranche
-echo "Flattening # Jtranche contract"
-npx truffle-flattener ./contracts/tranches/JProtocol.sol | awk '/SPDX-License-Identifier/&&c++>0 {next} 1' | awk '/pragma experimental ABIEncoderV2;/&&c++>0 {next} 1' | awk '/pragma solidity/&&c++>0 {next} 1' | awk '/author/&&c++>0 {next} 1' | awk '/summary/&&c++>0 {next} 1' | awk '/Created on/&&c++>0 {next} 1' | sed '/^[[:blank:]]*\/\/ File/d;s/#.*//' >./dist/JProtocol.sol
+# JCompound
+echo "Flattening # JCompound contract"
+npx truffle-flattener ./contracts/JCompound.sol | awk '/SPDX-License-Identifier/&&c++>0 {next} 1' | awk '/pragma experimental ABIEncoderV2;/&&c++>0 {next} 1' | awk '/pragma solidity/&&c++>0 {next} 1' | awk '/author/&&c++>0 {next} 1' | awk '/summary/&&c++>0 {next} 1' | awk '/Created on/&&c++>0 {next} 1' | sed '/^[[:blank:]]*\/\/ File/d;s/#.*//' >./dist/JCompound.sol
